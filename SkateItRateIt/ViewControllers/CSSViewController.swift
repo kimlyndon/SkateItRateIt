777a3a2dfb75db8c8d11fd01e2dea4a41b2daf0e
kimlyndon <codeculture101@gmail.com>
2019-01-05 19:01:09 -0500

    Add: CSSVC website

---
+++ SkateItRateIt/ViewControllers/CSSViewController.swift
@@ -0,0 +1,31 @@
+//
+//  CSSViewController.swift
+//  SkateItRateIt
+//
+//  Created by Kim Lyndon on 1/5/19.
+//  Copyright © 2019 Kim Lyndon. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class CSSViewController: UIViewController {
+    
+    @IBOutlet weak var webView: WKWebView!
+    
+    
+    // MARK: Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        
+        let url = URL(string: "https://www.ebay.com/str/californiaskateboardsupply")
+        let request = URLRequest(url: url!)
+        webView.load(request)
+    }
+    
+}
