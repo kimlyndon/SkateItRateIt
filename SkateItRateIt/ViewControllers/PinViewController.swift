410b877c2fc161f9b6078b2a14614fd3de727970
kimlyndon <codeculture101@gmail.com>
2019-01-06 14:43:05 -0500

    Add: Navigation Controller Add: PinViewController

---
+++ SkateItRateIt/ViewControllers/PinViewController.swift
@@ -0,0 +1,14 @@
+//
+//  PinViewController.swift
+//  SkateItRateIt
+//
+//  Created by Kim Lyndon on 1/6/19.
+//  Copyright © 2019 Kim Lyndon. All rights reserved.
+//
+
+import UIKit
+
+class PinViewController: UIViewController {
+    
+    
+}
