8a07a0d4dfe37590ae2a1d6eeba168e9486dfdb3
kimlyndon <codeculture101@gmail.com>
2019-01-08 13:38:59 -0500

    Add: pods to pod file and connect Firebase and Kingfisher

---
+++ SkateItRateIt/ViewControllers/PhotoViewController.swift
@@ -0,0 +1,15 @@
+//
+//  PhotoViewController.swift
+//  SkateItRateIt
+//
+//  Created by Kim Lyndon on 1/8/19.
+//  Copyright © 2019 Kim Lyndon. All rights reserved.
+//
+
+import UIKit
+import Kingfisher
+
+class PhotoViewController: UICollectionViewController {
+    
+    
+}
