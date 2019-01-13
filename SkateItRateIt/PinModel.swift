4f3757947688ef05c1516e6014e9c9578b249388
Fahad Shafique <fahad655@gmail.com>
2019-01-13 18:55:40 -0500

    Added instruction / Comments.

--- SkateItRateIt/PinModel.swift
+++ SkateItRateIt/PinModel.swift
@@ -9,7 +9,13 @@
 // This file is for the Firebase Realtime Database JSON data.
 
 class PinModel {
-    
+    /*
+     NOTE KIM: we don't need to make struct in class.
+     we can just make properties here (if you don't know properties please read about them.. its a must to know)
+     once you have created proper properties.. you can make object of this class and once you have made the object
+     you acn access the properties and then set the values to store after fetching them from firebase.
+     Let me know if you wnat more understanding about what i am saying. 
+     */
     struct Pin {
         static let latitude = "latitude"
         static let longitude = "longitude"
--- SkateItRateIt/ViewControllers/MapViewController.swift
+++ SkateItRateIt/ViewControllers/MapViewController.swift
@@ -12,14 +12,29 @@ import CoreLocation
 import Firebase
 
 
-class MapViewController: UIViewController, MKMapViewDelegate, UIGestureRecognizerDelegate {
+/*
+ NOTE KIM:
+ 
+ Here are some points on how the Firebase works.  
+- Everything on firebase (FB) has node (firebase calls it “chid” ) and its value.
+- FB sends and receives values as key value pairs.
+- You can fetch and put any value of the node by  giving reference of the specific node key (e.g. Pins).
+- While fetching /sending node ’s value every child node and their corresponding values will be fetch/send all at once ( usually regarded as snapshot ).
+- If a specific node does not exist and you try to send it, it ll create a new node (entry). If the node who’s value you are sending exist already, i ll override the existing value (sort of editing).
+- Database (who’s reference you get) does not store images (files), for storing images (files) you ll use  https://firebase.google.com/docs/storage/ and you ll upload files by reading here https://firebase.google.com/docs/storage/ios/upload-files once you have uploaded a file you ll store the url of the file in you FB database. so there are two different things. FB Database and FB Storage.
+ 
+ */
 
+
+
+class MapViewController: UIViewController, MKMapViewDelegate, UIGestureRecognizerDelegate {
+    
     @IBOutlet weak var mapView: MKMapView!
     
     var ref: DatabaseReference!
-    var pinInfo = [PinModel]()
+    var pinInfo = [PinModel]() //  NOTE KIM:  you are creating array here, it should be named pin array .. pin info is not right property name.
     
-    let newPin =  MKPointAnnotation()
+    let newPin =  MKPointAnnotation() //  NOTE KIM:  you don't specifically need to have a class property here, just create a pin and add it to map's pins.
     let locationManager = CLLocationManager()
     let regionInMeters: Double = 10000
     
@@ -29,15 +44,25 @@ class MapViewController: UIViewController, MKMapViewDelegate, UIGestureRecognize
         Database.database().isPersistenceEnabled = true
         ref = Database.database().reference()
         checkLocationServices()
+        
+        /*
+             NOTE KIM: if you want to fetch all of the pins do
+            self.ref.child("Pins").observeSingleEvent(of: .value, with: { (snapshot) in
+        })
+         
+        - once you have loaded all of the pins you can drop them to the map by a simple add pin method as you ll ilterate the pins you have loaded. e.g. for pin in loadedPins 
+         */
+        
+      
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
     }
-        
-        func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWithOtherGestureRecognizer: UIGestureRecognizer) -> Bool {
-            return true
-        }
+    
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWithOtherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return true
+    }
     
     func setupLocationManager() {
         locationManager.delegate = self
@@ -87,9 +112,13 @@ class MapViewController: UIViewController, MKMapViewDelegate, UIGestureRecognize
     
     //Drop pin to current location
     @IBAction func dropIn(_ sender: UIBarButtonItem) {
-         mapView.addAnnotation(newPin)
+        mapView.addAnnotation(newPin) //
         ref.childByAutoId().setValue([pinInfo]) //TODO: Find the right way to reference database!
         
+        /*
+             NOTE KIM: For sending data with "Pins" head node
+            self.ref.child("Pins").childByAutoId().setValue(["location":["Lat":35.23344,"long":-80.85261]])
+        */
     }
     
     // Long press pin drop
@@ -104,13 +133,13 @@ class MapViewController: UIViewController, MKMapViewDelegate, UIGestureRecognize
             
             //add annotation to map
             self.mapView.addAnnotation(annotation)
-             ref.childByAutoId().setValue([pinInfo]) //TODO: Find the right way to reference database!
+            ref.childByAutoId().setValue([pinInfo]) //TODO: Find the right way to reference database!
         }
     }
 }
 
 extension MapViewController: CLLocationManagerDelegate {
-   
+    
     // Update as user moves.
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         // Last known location. If no location use guard statement
@@ -118,7 +147,14 @@ extension MapViewController: CLLocationManagerDelegate {
         let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
         let region = MKCoordinateRegion.init(center: center, latitudinalMeters: regionInMeters, longitudinalMeters: regionInMeters)
         mapView.setRegion(region, animated: true)
-        
+        /*  NOTE KIM:
+           mapView.setRegion(region, animated: true)
+          - you don't need to set map's location here all of the time this delegate method is called. it is updated on user's device location change and you can only use it for storing user's last location only.
+          - For showing user's location on map just call a method of map to show user's location.
+         
+            newPin.coordinate = location.coordinate
+          - Again don't store pin's location like that, you got to drop pin on visible map's centre and not on user location's current location, i believe.
+        */
         newPin.coordinate = location.coordinate
         
     }
