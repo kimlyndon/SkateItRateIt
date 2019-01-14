31149c77ca09c5901aeb3343252f9d05940d5e63
kimlyndon <codeculture101@gmail.com>
2019-01-14 14:35:57 -0500

    Update: PhotoEditorVC

--- SkateItRateIt/PhotoEditorViewController.swift
+++ SkateItRateIt/PhotoEditorViewController.swift
@@ -8,9 +8,11 @@
 
 import UIKit
 
-class PhotoEditorViewController: UIViewController {
+class PhotoEditorViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate  {
 
     @IBOutlet weak var imagePickerView: UIImageView!
+    @IBOutlet weak var backButton: UIBarButtonItem!
+    @IBOutlet weak var editButton: UIBarButtonItem!
     @IBOutlet weak var camera: UIBarButtonItem!
     @IBOutlet weak var photoAlbumButton: UIBarButtonItem!
     @IBOutlet weak var shareButton: UIBarButtonItem!
@@ -38,6 +40,32 @@ class PhotoEditorViewController: UIViewController {
         present(imagePicker, animated: true, completion: nil)
     }
     
+    func generateImage() -> UIImage {
+        
+        // Render view to an image
+        UIGraphicsBeginImageContext(self.view.frame.size)
+        view.drawHierarchy(in: self.view.frame, afterScreenUpdates: true)
+        let newImage:UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        
+        return newImage
+    }
+    
+    func save() {
+        //TODO: save images when shared button utilized.
+        
+    }
+    
+    @IBAction func backButtonPressed(_ sender: UIBarButtonItem) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func editButtonPressed(_ sender: UIBarButtonItem) {
+        //TODO: Code for editing
+    }
+    
+    
     @IBAction func cameraButtonPressed() {
       pick(sourceType: .camera)
     }
@@ -49,17 +77,16 @@ class PhotoEditorViewController: UIViewController {
     }
     
     @IBAction func shareButtonPressed(_ sender: UIBarButtonItem) {
+        let images = generateImage()
+        let activityViewController = UIActivityViewController(activityItems: [images], applicationActivities: nil)
+        activityViewController.completionWithItemsHandler = {activity, didComplete, item, error in if didComplete {
+            self.save()
+            self.dismiss(animated: true, completion: nil)
+            self.navigationController?.popViewController(animated: true)
+            }
+        }
+        present(activityViewController, animated: true, completion: nil)
+        
+        
     }
 }
-
-extension PhotoEditorViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-    
-    /*private func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String: Any]) {
-        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
-        imagePickerView.image = image
-    }
-    
-    dismiss(animated: true, completion: nil)
-    }*/
-    }
-
