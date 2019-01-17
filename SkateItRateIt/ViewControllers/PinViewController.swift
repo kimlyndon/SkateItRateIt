8eb939596ac3782591af6939ae848c243414e5ab
kimlyndon <codeculture101@gmail.com>
2019-01-17 17:26:00 -0500

    Add: Begin pickerView code

--- SkateItRateIt/ViewControllers/PinViewController.swift
+++ SkateItRateIt/ViewControllers/PinViewController.swift
@@ -8,19 +8,53 @@
 
 import UIKit
 
-class PinViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class PinViewController: UIViewController, UIImagePickerControllerDelegate, UIPickerViewDelegate, UIPickerViewDataSource, UINavigationControllerDelegate {
     
+    //Number of columns of data in picker
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    //Number of rows of data in picker
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
     
     
     
     @IBOutlet weak var doneButton: UIBarButtonItem!
+    @IBOutlet weak var picker: UIPickerView!
     
- 
-  
+    var pickerData: [String] = [String]()
     
-    @IBAction func doneButtonPressed(_ sender: UIBarButtonItem) {
-       dismiss(animated: true, completion: nil)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
+        //Connect data
+        self.picker.delegate = self
+        self.picker.dataSource = self
+        
+        //Input the data into the array
+        pickerData = [""] //TODO: Determine review options for positive, neutral, and negative reviews.
     }
     
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+        // The data to return for the row and component (column) that's being passed in
+        func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+            return pickerData[row]
+        }
+        
+        // Capture the picker view selection
+        func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        }
+    }
+    
+    
+    @IBAction func doneButtonPressed(_ sender: UIBarButtonItem) {
+        dismiss(animated: true, completion: nil)
+        
+    }
 }
