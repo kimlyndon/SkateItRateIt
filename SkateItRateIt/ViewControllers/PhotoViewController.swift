485af2f2b5faa5d513bf11a2f06c77c5250b3216
kimlyndon <codeculture101@gmail.com>
2019-01-17 17:36:07 -0500

    Add: commented code for collection view

--- SkateItRateIt/ViewControllers/PhotoViewController.swift
+++ SkateItRateIt/ViewControllers/PhotoViewController.swift
@@ -40,4 +40,54 @@ class PhotoViewController: UICollectionViewController {
         
     }
     
+ /*   // MARK: - COLLECTION VIEW
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let space:CGFloat = 8.0
+        let dimension = (view.frame.size.width - (2 * space)) / 3.0
+        let dimension2 = (view.frame.size.height - (2 * space)) / 3.0
+        
+        return CGSize(width: dimension, height: dimension2)
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        print("***Collection View: Number of items in section***")
+        return photoView.numberOfObjects ?? 3
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        if indexPath.item == 3 {
+            print("***Collection View: Cell For Row at Index Path***")
+        }
+        
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "photoCell", for: indexPath) as! PhotoCell
+        
+        
+        
+        let activityIndicator = UIActivityIndicatorView(style: .whiteLarge)
+        activityIndicator.frame = cell.bounds
+        cell.backgroundColor = UIColor.darkGray
+        cell.imageView.alpha = 0.5
+        cell.addSubview(activityIndicator)
+        cell.imageView.image = #imageLiteral(resourceName: "VirtualTourist_1024")
+        activityIndicator.startAnimating()
+        
+        let aPhoto = image.object(at: indexPath)
+        
+        if aPhoto.image != nil {
+            
+            cell.photoView.image = UIImage(data: aPhoto.image!)
+            cell.photoView.alpha = 1.0
+            activityIndicator.stopAnimating()
+            activityIndicator.hidesWhenStopped = true
+            return cell
+            
+        } else { */
+            
+    
 }
+
