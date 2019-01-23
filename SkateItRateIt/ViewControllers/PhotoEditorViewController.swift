d06962158a537cbf6b88d199c1298a60334521ac
Fahad Shafique <fahad655@gmail.com>
2019-01-23 00:48:26 -0500

    Made changes for proper storage of image to firebase.

--- SkateItRateIt/ViewControllers/PhotoEditorViewController.swift
+++ SkateItRateIt/ViewControllers/PhotoEditorViewController.swift
@@ -14,7 +14,7 @@ class PhotoEditorViewController: UIViewController,  UINavigationControllerDelega
     var storageRef: StorageReference!
     let storage = Storage.storage()
     let imageCache = NSCache<NSString, UIImage>()
-
+    
     @IBOutlet weak var imagePickerView: UIImageView!
     @IBOutlet weak var backButton: UIBarButtonItem!
     @IBOutlet weak var saveButton: UIBarButtonItem!
@@ -59,15 +59,15 @@ class PhotoEditorViewController: UIViewController,  UINavigationControllerDelega
     
     
     @IBAction func saveButtonPressed(_ sender: UIBarButtonItem) {
-     let pictureViewController = PhotoViewController()
+        let pictureViewController = PhotoViewController()
         present(pictureViewController, animated: true, completion: nil)
         
     }
     
     
     @IBAction func cameraButtonPressed() {
-      pick(sourceType: .camera)
-    
+        pick(sourceType: .camera)
+        
     }
     
     @IBAction func pickAnImageFromAlbum(_ sender: UIBarButtonItem) {
@@ -92,18 +92,18 @@ class PhotoEditorViewController: UIViewController,  UINavigationControllerDelega
 }
 
 extension PhotoEditorViewController: UIImagePickerControllerDelegate {
-
+    
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         
         // constant to hold the information about the photo
-      if let photo = info[.originalImage] as? UIImage {
-         let photoData = photo.jpegData(compressionQuality: 0.8)
-            
-            // call function to upload photo
-            uploadPhotos(photoData: photoData as! Data)
+        if let photo = info[.originalImage] as? UIImage {
+            if  let photoData = photo.jpegData(compressionQuality: 0.8) {
+                // call function to upload photo
+                uploadPhotos(photoData: photoData)
+            }
+            picker.dismiss(animated: true, completion: nil)
         }
-        picker.dismiss(animated: true, completion: nil)
     }
     
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
@@ -112,24 +112,45 @@ extension PhotoEditorViewController: UIImagePickerControllerDelegate {
     
     func uploadPhotos(photoData: Data) {
         
-        // build a path using the user’s ID and a timestamp
-        let imagePath = "spot_photos/" + "/\(Double(Date.timeIntervalSinceReferenceDate * 1000)).jpg"
+        // Create a reference to the file you want to upload
+        let riversRef = storageRef.child("spot_photos/" + "/\(Double(Date.timeIntervalSinceReferenceDate * 1000)).jpg")
         
-        // set content type to “image/jpeg” in firebase storage metadata
-        let metadata = StorageMetadata()
-        metadata.contentType = "image/jpeg"
-        
-        // create a child node at imagePath with imageData and metadata
-        storageRef!.child(imagePath).putData(photoData, metadata: metadata) { (metadata, error) in
-            if error != nil {
-                print("Error uploading photo")
+        // Upload the file to the path "images/rivers.jpg"
+        riversRef.putData(photoData, metadata: nil) { (metadata, error) in
+            guard let metadata = metadata else {
+                // Uh-oh, an error occurred!
                 return
             }
-            // add imageURL to database
-           // self.saveButton([PinInfo.photoUrl: self.storageRef!.child((metadata?.path)!).description])
+            // Metadata contains file metadata such as size, content-type.
+            print ("size: ",metadata.size)
+            // You can also access to download URL after upload.
+            riversRef.downloadURL { (url, error) in
+                guard let downloadURL = url else {
+                    // Uh-oh, an error occurred!
+                    return
+                }
+                print("download url is: ", downloadURL) // you need to store this url to your pin model object's photoUrl array. and that sync that model with firebase.
+            }
+            
+            
+             /*       let imagePath = "spot_photos/" + "/\(Double(Date.timeIntervalSinceReferenceDate * 1000)).jpg"
+            
+                    // set content type to “image/jpeg” in firebase storage metadata
+                    let metadata = StorageMetadata()
+                    metadata.contentType = "image/jpeg"
+            
+                    // create a child node at imagePath with imageData and metadata
+                    storageRef!.child(imagePath).putData(photoData, metadata: metadata) { (metadata, error) in
+                        if error != nil {
+                            print("Error uploading photo")
+                            return
+                        }
+                        // add imageURL to database
+                        // self.saveButton([PinInfo.photoUrl: self.storageRef!.child((metadata?.path)!).description])
+                    }*/
         }
-}
-
+    }
+    
 }
 
 
